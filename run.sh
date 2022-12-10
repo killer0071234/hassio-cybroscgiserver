17ee53faa2bd5e28513e46d26edc73893b4ce178
Daniel Gangl <31815106+killer0071234@users.noreply.github.com>
2022-12-10 22:09:34 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/with-contenv bashio
+
+set -e
+echo "Starting nginx server..."
+nginx
+cd /usr/local/bin/scgi_server/src
+echo "Starting cybroscgiserver..."
+python app.py
+RETVAL=$?
+exit $RETVAL
